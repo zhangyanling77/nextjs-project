95b98f80afb0777dd095effad5f539c7cda5fc80
zhangyanling77 <18783226594@163.com>
2020-01-19 22:21:14 +0800

    Update profile.tsx

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -2,7 +2,9 @@ import { Button, Layout } from 'antd';
 import axios from '../utils/axios';
 import { connect } from 'react-redux'
 import router from 'next/router';
+
 const { Content } = Layout;
+
 function Profile(props) {
     return (
         <Content>
@@ -17,7 +19,7 @@ Profile.getInitialProps = async function (ctx) {
     let options: any = {
         url: '/api/currentUser'
     }
-    //如果此方法是在服务器执行的，那么会有ctx.req属性，它代表本次node请求对象
+    
     if (ctx.req && ctx.req.headers.cookie) {
         options.headers = options.headers || {};
         options.headers.cookie = ctx.req.headers.cookie;
