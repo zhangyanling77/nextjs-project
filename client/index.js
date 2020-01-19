4cae5f1f2a409eb24fc3eda8877f942ded39c14a
zhangyanling77 <18783226594@163.com>
2020-01-19 22:20:15 +0800

    Update index.js

--- client/index.js
+++ client/index.js
@@ -1,18 +1,16 @@
 const koa = require('koa');
 const Router = require('koa-router');
-let next = require('next');
+const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
-// handler是一个处理函数，我们可以把请求和响应传递给它，由它来进行最后的响应，渲染组件
+
 const handler = app.getRequestHandler();
-// port
 const PORT = 3000;
 
 app.prepare().then(() => {
     const server = new koa();
-    let router = new Router();
-    // 在这里定义路由
+    const router = new Router();
     server.use(router.routes());
 
     server.use(async (ctx, next) => {
